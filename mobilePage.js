c367a3e921b24821f35750534a7ac06ca352414e
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2023-03-02 13:26:20 -0600

    Indentation & Linting tests

--- mobilePage.js
+++ mobilePage.js
@@ -1,32 +1,28 @@
-let cards = document.getElementsByClassName("stylizer");
-let cardnum = 1;
+const cards = document.getElementsByClassName('stylizer')
+let cardnum = 1
 
-window.onresize = windowCheck;
+window.onresize = windowCheck
 
-function changeCard(){
-    cards[cardnum].classList.replace("z1", "z0")
-    if (cardnum >= 2){
-        cardnum = 0;
-    }else{
-        cardnum++;
-    };
-    cards[cardnum].classList.replace("z0", "z1");
+function changeCard () {
+  cards[cardnum].classList.replace('z1', 'z0')
+  if (cardnum >= 2) {
+    cardnum = 0
+  } else {
+    cardnum++
+  };
+  cards[cardnum].classList.replace('z0', 'z1')
 };
 
-function windowCheck(){
-    // for(let i = 0; i <= cards.length; i++){
-    //     console.log("Test")
-
-    // };
-    if(window.innerWidth < 800 && cards[0].classList.contains("animateLeft") || window.innerWidth < 800 && cards[2].classList.contains("animateRight")){
-        console.log("removed")
-        cards[0].classList.remove("animateLeft");
-        cards[2].classList.remove("animateRight");
-    }else if(window.innerWidth >= 800 && !cards[0].classList.contains("animateLeft") || window.innerWidth >= 800 && !cards[2].classList.contains("animateRight")){
-        console.log("added")
-        cards[0].classList.add("animateLeft");
-        cards[2].classList.add("animateRight");
-    }else{
-        return;
-    };
+function windowCheck () {
+  if ((window.innerWidth < 800 && cards[0].classList.contains('animateLeft')) || (window.innerWidth < 800 && cards[2].classList.contains('animateRight'))) {
+    console.log('removed')
+    cards[0].classList.remove('animateLeft')
+    cards[2].classList.remove('animateRight')
+  } else if ((window.innerWidth >= 800 && !cards[0].classList.contains('animateLeft')) || (window.innerWidth >= 800 && !cards[2].classList.contains('animateRight'))) {
+    console.log('added')
+    cards[0].classList.add('animateLeft')
+    cards[2].classList.add('animateRight')
+  } else {
+    return
+  };
 };
