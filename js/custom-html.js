55bda02fc6395f13fa013a53d42b2565574fe937
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2023-05-02 09:53:01 -0500

    Local Font + Draggable Windows

--- js/custom-html.js
+++ js/custom-html.js
@@ -1,12 +1,12 @@
-class win_98_nav extends HTMLElement{
+class win_98_win_nav extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `<h2>${this.dataset.title}</h2><div><button aria-label='minimize' /><button aria-label='maximize' /><button aria-label='close' /><div>`
     }
 }
 
-class win_98 extends HTMLElement{
+class win_98_win_main extends HTMLElement{
     connectedCallback() {
-        this.innerHTML = ""
+        this.innerHTML = "<div class='drag'></div>"
     }
 }
 
@@ -16,6 +16,6 @@ class win_98_desktop_icon extends HTMLElement{
     }
 }
 
-customElements.define('win-nav', win_98_nav)
-customElements.define('win-main', win_98)
+customElements.define('win-nav', win_98_win_nav)
+customElements.define('win-main', win_98_win_main)
 customElements.define('desktop-icon', win_98_desktop_icon)
--- js/window-manager.js
+++ js/window-manager.js
@@ -1,20 +1,32 @@
-let windows = document.getElementsByTagName("win-main")
+let windows = document.querySelectorAll("win-main")
+let desktop = document.getElementsByTagName("desktop-icon")
+let currentFocus = document.querySelector("win-main")
+
+windows.forEach(element => {
+    dragElement(element);
+    resize(element);
+})
 
 
 // Code to open windows on desktop icon click #############################################################################################
 document.querySelectorAll("desktop-icon").forEach(element => {
     element.addEventListener("click", event => {
 
-        console.log(element)
-        if(element.dataset.text === "About" && !windows[0].classList.contains("open")){
-            windows[0].classList.add("open")
-        }else if(element.dataset.text === "Websites"){
-            windows[1].classList.add("open")
-        }else if(element.dataset.text === "Socials"){
-            windows[2].classList.add("open")
-        }else if(element.dataset.text === "Projects"){
-            windows[3].classList.add("open")
+        for (i = 0; i < desktop.length; i++) {
+            if (desktop[i] === element){
+                changeFocus(windows[i])
+                if(!windows[i].classList.contains("open")) {
+                    windows[i].classList.add("open")
+                }
+            }
         }
+    })
+});
+
+document.querySelectorAll("win-main").forEach(element => {
+    element.addEventListener("mousedown", event => {
+
+        changeFocus(element)
 
     })
 });
@@ -23,9 +35,9 @@ document.querySelectorAll("button[aria-label='close']").forEach(element => {
     element.addEventListener("click", event => {
         let currwindow = element.parentElement.parentElement.parentElement
 
-        if(currwindow.classList.contains("open")){
+        if (currwindow.classList.contains("open")) {
             currwindow.classList.remove("open")
-            if(currwindow.classList.contains("max")){
+            if (currwindow.classList.contains("max")) {
                 currwindow.classList.remove("max")
             }
         }
@@ -36,10 +48,106 @@ document.querySelectorAll("button[aria-label='maximize']").forEach(element => {
     element.addEventListener("click", event => {
         let currwindow = element.parentElement.parentElement.parentElement
 
-        if(currwindow.classList.contains("max")){
-            currwindow.classList.remove("max")
-        }else{
+        if (!currwindow.classList.contains("max")) {
             currwindow.classList.add("max")
+            currwindow.style.left = 0
+            currwindow.style.top = 0
+        } else {
+            currwindow.classList.remove("max")
         }
     })
 });
+
+function changeFocus(to){
+
+    if(to === currentFocus){return}
+    console.log(to)
+    to.style.zIndex = parseInt(to.style.zIndex) + 10
+    currentFocus.style.zIndex = parseInt(currentFocus.style.zIndex) - 10
+    currentFocus = to
+}
+
+
+// i was lazy
+// https://www.w3schools.com/howto/howto_js_draggable.asp
+function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    if (elmnt.querySelector("win-nav")) {
+        // if present, the header is where you move the DIV from:
+        elmnt.querySelector("win-nav").onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        if (elmnt.classList.contains('max')) { return }
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        if((elmnt.offsetTop - pos2) < 0){pos2 = 0}
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function resize(elmnt) {
+    let sca1 = 0, sca2 = 0, sca3 = 0, sca4 = 0;
+
+    if (elmnt.querySelector("win-nav")) {
+        // if present, the header is where you move the DIV from:
+        elmnt.querySelector(".drag").onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        if (elmnt.classList.contains('max')) { return }
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        if((elmnt.offsetTop - pos2) < 0){pos2 = 0}
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
