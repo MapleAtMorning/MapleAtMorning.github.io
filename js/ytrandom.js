b5d893cbd32055983034d4ce68b8b3f9475c9007
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2024-02-13 22:20:07 -0600

    Update ytrandom.js

--- js/ytrandom.js
+++ js/ytrandom.js
@@ -1,15 +1,25 @@
 let rawJSON
+let keys
 async function jsonGrab(){
     const result = await fetch("https://mapleatmorning.github.io/ytrandomjson/ytrandom.json")
     rawJSON = await result.json()
     return rawJSON
 }
-await jsonGrab()
 
-let keys = Object.keys(rawJSON)
+async function refreshKeys(){
+    await jsonGrab()
+    keys = Object.keys(rawJSON)
+}
 
-function grabRandom(){
-    let randomItem = keys[Math.floor(Math.random()*keys.length)]
-    window.open(randomItem)
+async function grabRandom(){
+    if (!keys || keys.length <= 0){ // I know this technically misses a video, but I'm fine with that.
+        await refreshKeys()
+    }
+    let randNum = Math.floor(Math.random()*keys.length)
+    let randomItem = keys[randNum]
+    keys.splice(randNum, 1)
+    // console.log(`Keys Length: ${keys.length} \nRandom Number: ${randNum}`)
+    document.getElementById('random-link').href = randomItem
 }
 document.getElementById('startbtn').addEventListener("click", grabRandom)
+grabRandom()
